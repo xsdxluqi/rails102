cba9644cebe32f6aa3e9c51f1b9fd348012af22c
liuqi <xsdxluqi3class@gmail.com>
2016-12-30 10:03:45 -0800

    use before_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
 before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 
   def index
@@ -10,13 +10,9 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
    @group = Group.find(params[:id])
  end
 
- def edit
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
+  def edit
   end
+
 
    def new
    @group = Group.new
@@ -34,29 +30,19 @@ before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   end
 
   def update
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
     if @group.update(group_params)
       redirect_to groups_path, notice: "Update Success"
     else
       render :edit
     end
   end
+    
 
-  def destroy
-    @group = Group.find(params[:id])
-
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
-
+    def destroy
     @group.destroy
     redirect_to groups_path, alert: "Group deleted"
   end
+
 
  private
 
