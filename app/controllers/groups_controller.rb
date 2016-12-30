183ab9f2b7401c0ad5e9d322b9e2f603e696c4b8
liuqi <xsdxluqi3class@gmail.com>
2016-12-30 09:35:56 -0800

    check owner permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
   end
@@ -9,8 +11,12 @@ class GroupsController < ApplicationController
  end
 
  def edit
-  @group = Group.find(params[:id])
- end
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+  end
 
    def new
    @group = Group.new
@@ -28,23 +34,39 @@ class GroupsController < ApplicationController
   end
 
   def update
-     @group = Group.find(params[:id])
-     if @group.update(group_params)
-     redirect_to groups_path, notice: "Update Success"
-     else
-     render :edit
-   end
-   end
+    @group = Group.find(params[:id])
 
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
 
   def destroy
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
     @group.destroy
-    flash[:alert] = "Group deleted"
-    redirect_to groups_path
+    redirect_to groups_path, alert: "Group deleted"
   end
 
  private
+
+    vdef find_group_and_check_permission
+    @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
+
 
  def group_params
    params.require(:group).permit(:title, :description)
